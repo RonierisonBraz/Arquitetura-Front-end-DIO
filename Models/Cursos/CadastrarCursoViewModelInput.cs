422c58c48f8e14d36eecebca9fa6802c41a7e823
RonierisonBraz <ronierison18@gmail.com>
2021-08-26 10:42:14 -0300

    Validações de entrada

--- Models/Cursos/CadastrarCursoViewModelInput.cs
+++ Models/Cursos/CadastrarCursoViewModelInput.cs
@@ -1,9 +1,14 @@
-﻿namespace Curso.WebApi.Models.Cursos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Curso.WebApi.Models.Cursos
 {
     public class CadastrarCursoViewModelInput
     {
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "A descrição do curso é obrigatório")]
         public string Descricao { get; set; }
     }
 }
